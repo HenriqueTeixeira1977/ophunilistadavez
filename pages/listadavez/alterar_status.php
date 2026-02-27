c3bc612e1900edd9f491fce0577322e78437e250
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-02-27 02:52:51 -0300

    260227025239-Update

--- pages/listadavez/alterar_status.php
+++ pages/listadavez/alterar_status.php
@@ -5,11 +5,37 @@ $id = $_GET['id'];
 $acao = $_GET['acao'];
 
 if($acao == 'pausar'){
+
+    // coloca como inativo
     $conn->query("UPDATE vendedores SET status='inativo' WHERE id=$id");
+
 }
 
 if($acao == 'ativar'){
+
+    // ativa vendedor
     $conn->query("UPDATE vendedores SET status='ativo' WHERE id=$id");
+
+    // pega última posição atual
+    $ultima = $conn->query("
+        SELECT MAX(posicao) as max FROM fila
+    ")->fetch_assoc()['max'];
+
+    // coloca ele na última posição
+    $conn->query("
+        UPDATE fila 
+        SET posicao = $ultima + 1
+        WHERE vendedor_id = $id
+    ");
+
+    // reorganiza fila
+    $conn->query("SET @p = 0");
+
+    $conn->query("
+        UPDATE fila
+        SET posicao = (@p := @p + 1)
+        ORDER BY posicao
+    ");
 }
 
 header("Location: index.php");
