63789f1818e2b7ef9cfd56a8b8b87ec4d904806e
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-02-28 09:04:17 -0300

    260228090323-Update

--- atendimentos/atendimentos.php
+++ atendimentos/atendimentos.php
@@ -1,4 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
+
+
+<?php
 require_once '../config/database.php';
 
 $usuario_id = $_SESSION['usuario_id'] ?? null;
