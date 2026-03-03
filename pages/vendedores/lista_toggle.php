caefd0bd4adfc79d75e48b1451a2fcc6497b827b
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-03-02 21:48:06 -0300

    260302214748-Update

--- pages/vendedores/lista_toggle.php
+++ pages/vendedores/lista_toggle.php
@@ -11,17 +11,16 @@ $id = intval($_GET['id']);
 $acao = $_GET['acao'] ?? '';
 
 if($acao === 'remover'){
-    $valor = 0;
-} elseif($acao === 'adicionar'){
-    $valor = 1;
-} else {
-    header("Location: index.php");
-    exit;
+    $stmt = $conn->prepare("UPDATE vendedores SET na_lista = 0 WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
 }
 
-$stmt = $conn->prepare("UPDATE vendedores SET na_lista = ? WHERE id = ?");
-$stmt->bind_param("ii", $valor, $id);
-$stmt->execute();
+if($acao === 'adicionar'){
+    $stmt = $conn->prepare("UPDATE vendedores SET na_lista = 1 WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+}
 
 header("Location: index.php");
 exit;
